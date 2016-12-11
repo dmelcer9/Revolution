5d2017b9dc9b316a9860000dc811df604065f8fc
Vlad Stelea <vladstelea@gmail.com>
2016-12-11 15:06:07 -0500

    Made baddies move

---
+++ Revolution/Assets/Scripts/Scene1.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scene1 : MonoBehaviour {
+    public float Baddie1Position;
+    public float BaddieRotateSpeed;
+    public Transform Baddie2Position;
+    public float BaddieMoveSpeed;
+    public GameObject BadGuy1;
+    public GameObject Badguy2;
+    public GameObject Soldier1;
+    public GameObject Soldier2;
+    public GameObject Dialogue;
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(moveBaddies());
+	}
+    bool moveOne = true;
+	// Update is called once per frame
+	void Update () {
+       
+	}
+    IEnumerator moveBaddies() {
+        while (BadGuy1.transform.position.x < Baddie1Position)
+        {
+            BadGuy1.transform.Translate(Vector3.right * BaddieMoveSpeed);
+            Badguy2.transform.Translate(Vector3.left * BaddieMoveSpeed);
+            yield return 0;
+        }
+         
+    }
+
+    
+}
